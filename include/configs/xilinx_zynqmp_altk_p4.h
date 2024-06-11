8d1685ae18d0d62e8577a4104fb53dc4a9f69a3a
Theo-Tsang <13242449415@163.com>
2024-06-11 23:50:20 +0800

    altk-zynqmp-p4 platform created

---
+++ include/configs/xilinx_zynqmp_altk_p4.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Configuration for Xilinx ZynqMP
+ * (C) Copyright 2024 Theo Tsang <sihao.tsang@gmail.com>
+ *
+ * Based on Altk ZynqMP P4 platform
+ */
+
+#ifndef __XILINX_ZYNQMP_ALTK_P4_H
+#define __XILINX_ZYNQMP_ALTK_P4_H
+
+#include <configs/xilinx_zynqmp.h>
+
+#endif /* __XILINX_ZYNQMP_ALTK_P4_H */
